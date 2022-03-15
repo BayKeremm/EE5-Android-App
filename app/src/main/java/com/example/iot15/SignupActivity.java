72b07824527f2b0d05cc7e6cccbbd18bbea3c38c
michiel.thiers <michiel.thiers@student.kuleuven.be>
2022-03-15 19:12:28 +0100

    signup implemented

--- app/src/main/java/com/example/iot15/SignupActivity.java
+++ app/src/main/java/com/example/iot15/SignupActivity.java
@@ -14,16 +14,15 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
-    EditText etUsername, etPassword;
+    EditText etUsername, etPassword, etRepeatPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        setContentView(R.layout.sign_up_screen);
 
         viewInitializations();
     }
@@ -31,11 +30,14 @@ public class LoginActivity extends AppCompatActivity {
     void viewInitializations() {
         etUsername = findViewById(R.id.et_username);
         etPassword = findViewById(R.id.et_password);
+        etRepeatPassword = findViewById(R.id.et_repeat_password);
+
+        // To show back button in actionbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     // Checking if the input in form is valid
     boolean validateInput() {
-
         if (etUsername.getText().toString().equals("")) {
             etUsername.setError("Please Enter Username");
             return false;
@@ -44,45 +46,45 @@ public class LoginActivity extends AppCompatActivity {
             etPassword.setError("Please Enter Password");
             return false;
         }
+        if (etRepeatPassword.getText().toString().equals("")) {
+            etRepeatPassword.setError("Please Enter Repeat Password");
+            return false;
+        }
 
+        // Checking if repeat password is same
+        if (!etPassword.getText().toString().equals(etRepeatPassword.getText().toString())) {
+            etRepeatPassword.setError("Password does not match");
+            return false;
+        }
         return true;
     }
 
     // Hook Click Event
 
-    public void performSignUp (View v) {
+    public void performSignUp(View v) {
         if (validateInput()) {
 
             // Input is valid, here send data to your server
 
             String username = etUsername.getText().toString();
             String password = etPassword.getText().toString();
+            String repeatPassword = etRepeatPassword.getText().toString();
 
-            checkPassword(username, password);
+            signUpSQL(username, password);
 
-            //Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
+            // Here you can call you API
+
         }
     }
 
-    public void goToSignup(View v) {
-        // Open your SignUp Activity if the user wants to signup
-        Intent intent = new Intent(this, SignupActivity.class);
-        startActivity(intent);
-    }
-
-    private void checkPassword(String username, String password) {
+    public void signUpSQL(String username, String password){
         RequestQueue queue= Volley.newRequestQueue(this);
-        String url="https://studev.groept.be/api/a21iot15/retrieve_by_username/" + username;
-        System.out.println(url);
+        String url="https://studev.groept.be/api/a21iot15/sign_up_user/" + username + "/" + password;
         StringRequest stringRequest=new StringRequest(Request.Method.GET, url, response -> {
             try {
-                JSONArray responseJSON = new JSONArray(response);
-                // compare passwords
-                if(responseJSON.getJSONObject(0).getString("username").compareTo(password) == 0){
-                    // move to next screen
-                    startActivity(new Intent(this, MainActivity.class));
+                    startActivity(new Intent(this, LoginActivity.class));
                 }
-            }
             catch (Exception e){
                 e.printStackTrace();
             }
@@ -90,5 +92,6 @@ public class LoginActivity extends AppCompatActivity {
         }, error -> System.out.println("error"));
 
         queue.add(stringRequest);
+
     }
 }
