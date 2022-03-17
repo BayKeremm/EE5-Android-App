95ccf71d798d1ac162c81726b50310b1bc68ad48
charutheunis <charu.theunis@student.kuleuven.be>
2022-03-17 23:43:02 +0100

    dd

--- app/src/main/java/com/example/iot15/AndroidListViewActivity.java
+++ app/src/main/java/com/example/iot15/AndroidListViewActivity.java
@@ -22,5 +22,22 @@ public class AndroidListViewActivity extends ListActivity {
         // Binding resources Array to ListAdapter
         this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, plant_types));
 
+        ListView lv = getListView();
+
+        // listening to single list item on click
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String product = ((TextView) view).getText().toString();
+
+                // Launching new Activity on selecting single List Item
+                Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+                // sending data to new activity
+                i.putExtra("product", product);
+                startActivity(i);
+            }
+        });
     }
 }
