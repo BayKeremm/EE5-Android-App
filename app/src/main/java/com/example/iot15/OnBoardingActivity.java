3d6d2749b537f3a07d19310a03c40ea21a842c49
Qunonoell <vlada.mekhryukova@student.kuleuven.be>
2022-05-10 16:38:54 +0200

    On boarding screens added

---
+++ app/src/main/java/com/example/iot15/OnBoardingActivity.java
@@ -0,0 +1,126 @@
+package com.example.iot15;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class OnBoardingActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+    Button startBtn;
+    Animation animation;
+    Button skipBtn;
+    ConstraintLayout constraintLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_on_boarding);
+
+
+        viewPager = findViewById(R.id.viewPager);
+        dotsLayout = findViewById(R.id.dots);
+        startBtn = findViewById(R.id.get_started_btn);
+        skipBtn = findViewById(R.id.skip_btn);
+        constraintLayout = findViewById(R.id.onboardLayout1);
+
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+
+        addDots(0);
+        setActivityBackgroundColor(0);
+        viewPager.addOnPageChangeListener(changeListener);
+    }
+
+    public void toSignUp(View view) {
+
+        startActivity(new Intent(getApplicationContext(), SignupActivity.class));
+        finish();
+
+    }
+
+    public void setActivityBackgroundColor(int position) {
+
+        if (position == 1){
+            View view = this.getWindow().getDecorView();
+            view.setBackgroundColor(getResources().getColor(R.color.artBackground2));
+            //constraintLayout.setBackground();
+        }else{
+            View view = this.getWindow().getDecorView();
+            view.setBackgroundColor(getResources().getColor(R.color.artBackground));
+        }
+
+    }
+
+
+    private void addDots(int position) {
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.colorText));
+
+            dotsLayout.addView(dots[i]);
+        }
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.black));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDots(position);
+            setActivityBackgroundColor(position);
+
+            if (position != 2) {
+                startBtn.setVisibility(View.INVISIBLE);
+                skipBtn.setVisibility(View.VISIBLE);
+            } else {
+                animation = AnimationUtils.loadAnimation(OnBoardingActivity.this, R.anim.bottom_anim);
+                animation.setDuration(1000);
+                startBtn.setAnimation(animation);
+                startBtn.setVisibility(View.VISIBLE);
+                skipBtn.setVisibility(View.INVISIBLE);
+            }
+
+            if(position == 1){
+                //View view = this.getWindow().getDecorView();
+                constraintLayout.setBackgroundColor(getResources().getColor(R.color.artBackground2));
+            }else{
+                constraintLayout.setBackgroundColor(getResources().getColor(R.color.artBackground));
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/example/iot15/SliderAdapter.java
@@ -0,0 +1,83 @@
+package com.example.iot15;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    int images[] = {
+            R.drawable.onboardingplant2,
+            R.drawable.onboardingplant3,
+            R.drawable.onboardingplant4
+    };
+    int headings[] = {
+            R.string.slider_title1,
+            R.string.slider_title2,
+            R.string.slider_title3
+    };
+
+    int texts[] = {
+            R.string.slider_text1,
+            R.string.slider_text2,
+            R.string.slider_text3
+    };
+
+    int textColor[] ={
+            R.color.colorText,
+            R.color.white,
+            R.color.colorText
+    };
+
+
+    @Override
+    public int getCount() {
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slides_layout, container, false);
+
+        ImageView imageView = view.findViewById(R.id.slider_img);
+        TextView title = view.findViewById(R.id.title_onboarding);
+        TextView text = view.findViewById(R.id.text_onboarding);
+
+        imageView.setImageResource(images[position]);
+        title.setText(headings[position]);
+        text.setText(texts[position]);
+//        title.setTextColor(textColor[position]);
+//        text.setTextColor(textColor[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout)object);
+    }
+}
